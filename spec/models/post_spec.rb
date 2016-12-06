228595f633a35df67952d146a36d7f405e229c22
Matthew <matthewwgalloway@gmail.com>
2016-12-05 22:02:15 -0600

    Add pending post model tests
    Added pending tests for testing the presence of a subject, content,
    and an author for the post model.

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,9 @@
+require_relative '../rails_helper'
+
+describe "Post" do
+  it "has a valid factory"
+  it "is invalid without a subject"
+  it "is invalid without content"
+  it "is invalid without an author"
+  it "should belong to an author"
+end
